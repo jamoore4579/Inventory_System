202941d37f9bd4fcbe9b6a86f1d17a1e57ccc669
jamoore4579 <jasonamoore@frontier.com>
2023-02-18 10:09:51 -0500

    setup verbose title headings

--- inventory_system/dashboard/models.py
+++ inventory_system/dashboard/models.py
@@ -13,6 +13,9 @@ class Product(models.Model):
     category = models.CharField(max_length=20, choices=CATEGORY, null=True)
     quantity = models.PositiveIntegerField(null=True)
 
+    class Meta:
+        verbose_name_plural = 'Product'
+
     def __str__(self):
         return f'{self.name}-{self.quantity}'
 
@@ -21,6 +24,9 @@ class Order(models.Model):
     staff = models.ForeignKey(User, models.CASCADE, null=True)
     order_quantity = models.PositiveIntegerField(null=True)
     date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'Order'
 
     def __str__(self):
         return f'{self.product} ordered by {self.staff.username}'
