1196d38d7140492cc20afb2532bf94b477b4d1f5
jamoore4579 <jasonamoore@frontier.com>
2023-02-25 23:58:02 -0500

    build registration POST

--- inventory_system/user/views.py
+++ inventory_system/user/views.py
@@ -1,8 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 
 
 # Create your views here.
 def register(request):
-    form = UserCreationForm()
-    return render(request, 'user/register.html', {'form': form})
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('dashboard-index')
+    else:
+        form = UserCreationForm()
+    context = {
+        'form':form
+    }
+    return render(request, 'user/register.html', context)
